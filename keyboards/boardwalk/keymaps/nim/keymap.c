0653805274a67bfad59516949c1eeb4ed09132b8
Nim <WemansG90@users.noreply.github.com>
2022-06-10 14:57:43 +0200

    put alt and gui on a less shitty key

--- keyboards/boardwalk/keymaps/nim/keymap.c
+++ keyboards/boardwalk/keymaps/nim/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_PGUP, KC_RALT, KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______, KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGDN, KC_SFTENT, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_DEL, \
-           LOWER,   RAISE, KC_LALT, KC_LGUI,       KC_SPC,         KC_BSPC,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT           \
+           KC_LGUI,   KC_LALT, LOWER, RAISE,       KC_SPC,         KC_BSPC,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT           \
   ),
 
   /* Keymap LOWER: Lower Layer */
